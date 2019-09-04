de01b002b865b97d132be4aa05bc2958728d250c
Sam Jespersen <heyheysamj@gmail.com>
2019-09-04 09:24:15 -0700

    components for about us

---
+++ data/about-us.js
@@ -0,0 +1,35 @@
+export const about = [
+    {
+        name: 'Sam',
+        image: './assets/img/sam-icon.png',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati a impedit, facere repudiandae beatae quae
+            minus nesciunt consequuntur rem ipsam id. Minima
+            incidunt quasi rerum eaque quos? Eaque, quae in?`
+    },
+    {
+        name: 'Angela',
+        image: '',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati a impedit, facere repudiandae beatae quae
+            minus nesciunt consequuntur rem ipsam id. Minima
+            incidunt quasi rerum eaque quos? Eaque, quae in?`
+    },
+    {
+        name: 'Dylan',
+        image: '',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati a impedit, facere repudiandae beatae quae
+            minus nesciunt consequuntur rem ipsam id. Minima
+            incidunt quasi rerum eaque quos? Eaque, quae in?`
+    },
+    {
+        name: 'Kam',
+        image: '',
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati a impedit, facere repudiandae beatae quae
+            minus nesciunt consequuntur rem ipsam id. Minima
+            incidunt quasi rerum eaque quos? Eaque, quae in?`
+    },
+
+];
