b0329383a5dd09343f54fe657cf39ada28a79ed5
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-04 16:44:44 -0700

    finished html and css for about us page, added img, deleted imgs, updated components

--- data/about-us.js
+++ data/about-us.js
@@ -1,35 +1,28 @@
 export const about = [
     {
+        name: 'Rick',
+        image: '../../public/assets/img/rick.jpg',
+        desc: `Oh, I'm sorry Morty, are you the scientist or are you the kid who wanted to get laid? Do you know how many characters there are in the Simpsons Morty? There's like a-a billion characters, M-Morty. There was an episode where Former President BUSH was their neighbor! Puh rum pum pow! Hello Jerry, come to rub my face in urine again?`
+    },
+    {
         name: 'Sam',
         image: './assets/img/sam-icon.png',
-        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Obcaecati a impedit, facere repudiandae beatae quae
-            minus nesciunt consequuntur rem ipsam id. Minima
-            incidunt quasi rerum eaque quos? Eaque, quae in?`
+        desc: `This is because you give Morty Smith bad grades, bitch! Get off the high road Summer. We all got pink eye because you wouldn't stop texting on the toilet. Slow down! I'm Mr. Crowbar, and here is my friend, who is also a crowbar!`
     },
     {
         name: 'Angela',
         image: '',
-        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Obcaecati a impedit, facere repudiandae beatae quae
-            minus nesciunt consequuntur rem ipsam id. Minima
-            incidunt quasi rerum eaque quos? Eaque, quae in?`
+        desc: `Oh, that's Million Ants. I can't see the ants from over here, I just assumed that was um.. Turd Man, Wiggly Turd Man. Alan Rails, ladies and gentlemen. After his parents' tragic death in a railroad accident, he gained the power to summon ghost trains. It's not all bad though, they were spared having to see their grown son wear a whistle. You have to turn them on Morty! The shoes need to be turned on! Is he keeping his shoulders square? Oooooooh he's tryin'!`
     },
     {
         name: 'Dylan',
         image: '',
-        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Obcaecati a impedit, facere repudiandae beatae quae
-            minus nesciunt consequuntur rem ipsam id. Minima
-            incidunt quasi rerum eaque quos? Eaque, quae in?`
+        desc: `Those guys are inside you building a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck those people, fuck this whole thing Ethan. Your blood will be my lotion! Lick, lick, lick my balls! I turned myself into a pickle, Morty! Boom! Big reveal! I'm a pickle! What do you think about that? I turned myself into a pickle! W-what are you just staring at me for, bro? I turned myself into a pickle, Morty.`
     },
     {
         name: 'Kam',
         image: '',
-        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Obcaecati a impedit, facere repudiandae beatae quae
-            minus nesciunt consequuntur rem ipsam id. Minima
-            incidunt quasi rerum eaque quos? Eaque, quae in?`
+        desc: `We will return… possibly in different clothing. Aw, c'mon Rick. That doesn't seem so bad. We all wanna die, we're meeseeks! It's a device Morty, that when you put it in your ear, you can enter people's dreams Morty. Its just like that movie that you keep crowing about.`
     },
 
 ];
