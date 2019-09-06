f1e7bdb0be981c2ed0bb0ef7c3091ee9ddc3c1b2
Sam Jespersen <heyheysamj@gmail.com>
2019-09-06 13:34:03 -0700

    cleanup

--- public/src/component/app/Type.js
+++ public/src/component/app/Type.js
@@ -1,21 +1,38 @@
 import Component from '../Component.js';
 
 class Type extends Component {
+
+    onRender(dom) {
+        const div = dom.querySelector('.type');
+        const desc = dom.querySelector('#desc');
+
+        div.addEventListener('mouseover', () => {
+            desc.classList.remove('hidden');
+        });
+        div.addEventListener('mouseout', () => {
+            desc.classList.add('hidden');
+        });
+
+
+    }
     renderHTML() {
         const props = this.props;
         return /*html*/`
+        <div id = "top-div">
             <div class="type">
                     <span>${props.name}</span>
                     <img
                         src="${props.image}"
                         title="${props.quote}">
                     <span>${props.personality}</span>
+                    <span id="desc" class="hidden">${props.description}</span>
                     <ul>
                         <li>Status: ${props.status}</li>
                         <li>Species: ${props.species}</li>
                         <li>Gender: ${props.gender}</li>
                         <li>Origin: ${props.origin}</li>
                     </ul>
+                </div>
                 </div>
         `;
     }
