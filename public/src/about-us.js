266ff3d12d444203346a544aa077012b0326d70c
Angela Vawser <amvawser@gmail.com>
2019-09-06 10:42:15 -0700

    updates to character data and about me info

--- public/src/about-us.js
+++ public/src/about-us.js
@@ -5,9 +5,9 @@ export const about = [
         desc: `This is because you give Morty Smith bad grades, bitch! Get off the high road Summer. We all got pink eye because you wouldn't stop texting on the toilet. Slow down! I'm Mr. Crowbar, and here is my friend, who is also a crowbar!`
     },
     {
-        name: 'Angela',
-        image: './assets/img/Angela-icon.png',
-        desc: `Oh, that's Million Ants. I can't see the ants from over here, I just assumed that was um.. Turd Man, Wiggly Turd Man. Alan Rails, ladies and gentlemen. After his parents' tragic death in a railroad accident, he gained the power to summon ghost trains. It's not all bad though, they were spared having to see their grown son wear a whistle. You have to turn them on Morty! The shoes need to be turned on! Is he keeping his shoulders square? Oooooooh he's tryin'!`
+        name: 'Kam',
+        image: './assets/img/kam-icon.png',
+        desc: `We will return… possibly in different clothing. Aw, c'mon Rick. That doesn't seem so bad. We all wanna die, we're meeseeks! It's a device Morty, that when you put it in your ear, you can enter people's dreams Morty. Its just like that movie that you keep crowing about.`
     },
     {
         name: 'Dylan',
@@ -15,9 +15,9 @@ export const about = [
         desc: `Those guys are inside you building a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck those people, fuck this whole thing Ethan. Your blood will be my lotion! Lick, lick, lick my balls! I turned myself into a pickle, Morty! Boom! Big reveal! I'm a pickle! What do you think about that? I turned myself into a pickle! W-what are you just staring at me for, bro? I turned myself into a pickle, Morty.`
     },
     {
-        name: 'Kam',
-        image: './assets/img/kam-icon.png',
-        desc: `We will return… possibly in different clothing. Aw, c'mon Rick. That doesn't seem so bad. We all wanna die, we're meeseeks! It's a device Morty, that when you put it in your ear, you can enter people's dreams Morty. Its just like that movie that you keep crowing about.`
+        name: 'Angela',
+        image: './assets/img/Angela-icon.png',
+        desc: `My result: Mr. Needful, AKA "The Devil" (ENTP).`
     },
     {
         name: 'Rick',
